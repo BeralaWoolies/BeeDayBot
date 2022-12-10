d71c3af301fe8081b481eb12d05a2bd00995c983
Clarke Tran <cynicalparadox4@gmail.com>
2022-12-10 19:04:12 +1100

    added script for deploying commands to discord client and implemented script to deploy commands

--- src/main.js
+++ src/main.js
@@ -42,4 +42,27 @@ client.on(Events.MessageCreate, msg => {
 	msg.reply(`Hello!`);
 });
 
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) {
+		return;
+	}
+
+	const command = interaction.client.commands.get(interaction.commandName);
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found`);
+		return;
+	}
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({
+			content: 'There was an error while executing this command!',
+			ephemeral: true
+		});
+	}
+
+});
+
 client.login(process.env.DISCORD_TOKEN);
