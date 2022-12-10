3ff028944195cc93bfbf05bc2a209da3caa43912
clarke tran <clarketran@Jingle.local>
2022-12-10 17:00:16 +1100

    barebones discord bot online

---
+++ src/main.js
@@ -0,0 +1,10 @@
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+const dotenv = require('dotenv').config();
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once(Events.ClientReady, (c) => {
+	console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+client.login(process.env.DISCORD_TOKEN);
