9a8b5a4b2fc89fbcaff7e0d37d7410650444190b
Clarke Tran <cynicalparadox4@gmail.com>
2022-12-10 19:49:46 +1100

    added event handling

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 const fs = require('node:fs');
 const path = require('node:path');
-const { Client, Events, GatewayIntentBits, Collection } = require('discord.js');
+const { Client, GatewayIntentBits, Collection } = require('discord.js');
 const dotenv = require('dotenv').config();
 
 const client = new Client({
@@ -13,16 +13,27 @@ const client = new Client({
 
 client.commands = new Collection();
 
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+console.log('event files found: ', eventFiles);
+
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
+
 const commandsPath = path.join(__dirname, 'commands');
-console.log(`commandsPath: ${commandsPath}`);
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 console.log('command files found: ', commandFiles);
 
 for (const file of commandFiles) {
 	const filePath = path.join(commandsPath, file);
-	console.log(`filePath: ${filePath}`);
 	const command = require(filePath);
-	console.log(command)
 	// Set a new item in the Collection with the key as the command name and the value as the exported module
 	if ('data' in command && 'execute' in command) {
 		client.commands.set(command.data.name, command);
@@ -30,39 +41,5 @@ for (const file of commandFiles) {
 		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
 	}
 }
-
-client.once(Events.ClientReady, c => {
-	console.log(`Ready! Logged in as ${c.user.tag}`);
-});
-
-client.on(Events.MessageCreate, msg => {
-	if(msg.author.id === client.user.id) {
-		return;
-	}
-	msg.reply(`Hello!`);
-});
-
-client.on(Events.InteractionCreate, async interaction => {
-	if (!interaction.isChatInputCommand()) {
-		return;
-	}
-
-	const command = interaction.client.commands.get(interaction.commandName);
-	if (!command) {
-		console.error(`No command matching ${interaction.commandName} was found`);
-		return;
-	}
-
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		await interaction.reply({
-			content: 'There was an error while executing this command!',
-			ephemeral: true
-		});
-	}
-
-});
 
 client.login(process.env.DISCORD_TOKEN);
